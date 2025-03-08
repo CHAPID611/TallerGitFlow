e3cbf5d819d103e6d9025766c101917220535172
EmerIlesS <emersonilesitp@gmail.com>
2025-03-08 18:46:30 -0500

    feat: Implementar botones de iniciar y detener del cronómetro
    - Agregar botón verde para iniciar el cronómetro
    - Agregar botón rojo para detener el cronómetro
    - Implementar lógica básica del cronómetro

--- src/renderer/src/components/InputField.jsx
+++ src/renderer/src/components/InputField.jsx
@@ -1,20 +1,22 @@
 // eslint-disable-next-line react/prop-types
-export default function InputField({ label, value, onChange, placeholder }) {
+export default function InputField({ label, value, onChange, placeholder, min, max }) {
   const handleInputChange = (e) => {
     const inputValue = e.target.value
-    if (/^\d+$/.test(inputValue)) {
+    if (inputValue === '' || (/^\d+$/.test(inputValue) && parseInt(inputValue) >= (min || 0) && parseInt(inputValue) <= (max || Infinity))) {
       onChange(e)
     }
   }
 
   return (
-    <div className="text-3xl">
-      <label className="text-stone-200">{label}:</label>
+    <div className="text-3xl mb-4">
+      <label className="text-stone-200 mr-2">{label}:</label>
       <input
         type="number"
         value={value}
         placeholder={placeholder}
-        className="w-20 bg-transparent text-blue-400"
+        min={min}
+        max={max}
+        className="w-20 bg-transparent text-blue-400 border-b-2 border-blue-400 focus:outline-none focus:border-blue-600"
         onChange={handleInputChange}
       />
     </div>
